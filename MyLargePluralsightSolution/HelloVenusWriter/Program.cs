a0740cfcfb9cb638b69ec0fea654a71dc1b0afc2
oroumand <ar.oroumand@gmail.com>
2019-08-06 11:12:26 +0430

    Visual Studio test

---
+++ MyLargePluralsightSolution/HelloVenusWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿using SharedComponents;
+using System;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyWriter writer = new MyWriter();
+   
+
+            DateTime earthTime = DateTime.Now;
+            double earthPrintTime = earthTime.AddSeconds(-2).Second;
+            var earthPrintTimeAfternoon = earthPrintTime + 12;
+
+            var firstEarthTime = writer.GetHelloVenusString();
+            var firstEarthTimeSplit = firstEarthTime.Split('\n');
+    
+        }
+    }
+}
