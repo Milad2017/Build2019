a569809c0836526fcb67a2565c5dae33dab2ff88
oroumand <ar.oroumand@gmail.com>
2019-08-07 14:03:19 +0430

    Blazor Test

---
+++ NikamoozBuild2019Web/NikamoozBuild2019Web.Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NikamoozBuild2019Web.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? DoneDate { get; set; }
+    }
+}
